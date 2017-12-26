84b835cd4eff168e2457e818414a8399e9348992
ERKİN İŞÇİ <erkin.isci@gmail.com>
2017-12-26 15:58:44 +0300

    NetMQ.Websocket project was updated for NetMQ version 4.0 and test projectsimplemented for test

---
+++ NetMQ.WebSockets/src/ConsoleApplication1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using NetMQ;
+using NetMQ.WebSockets;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WSRouter router = new WSRouter())
+            using (WSPublisher publisher = new WSPublisher())
+            {
+                router.Bind("ws://localhost:80");
+                publisher.Bind("ws://localhost:81");
+
+                router.ReceiveReady += (sender, eventArgs) =>
+                {
+                    var identity = eventArgs.WSSocket.ReceiveFrameBytes();
+                    string message = eventArgs.WSSocket.ReceiveFrameString();
+
+                    eventArgs.WSSocket.SendMoreFrame(identity).SendFrame("OK");
+
+                    publisher.SendMoreFrame("chat").SendFrame(message);
+                };
+
+                NetMQPoller poller = new NetMQPoller();
+                poller.Add(router);
+
+                poller.Run();
+            }
+        }
+    }
+}
---
+++ websocketmq/websocketmq/Program.cs
@@ -0,0 +1,35 @@
+﻿using NetMQ;
+using NetMQ.WebSockets;
+
+namespace websocketmq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var router = new WSRouter())
+            {
+                using (var publisher = new WSPublisher())
+                {
+                    router.Bind("ws://localhost:80");
+                    publisher.Bind("ws://localhost:81");
+
+                    router.ReceiveReady += (sender, eventArgs) =>
+                    {
+                        var identity = eventArgs.WSSocket.ReceiveFrameBytes();
+                        string message = eventArgs.WSSocket.ReceiveFrameString();
+
+                        eventArgs.WSSocket.SendMoreFrame(identity).SendFrame("OK");
+
+                        publisher.SendMoreFrame("chat").SendFrame(message);
+                    };
+
+                    NetMQPoller poller = new NetMQPoller();
+                    poller.Add(router);
+
+                    poller.Run();
+                }
+            }
+        }
+    }
+}
